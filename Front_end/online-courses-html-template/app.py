386400f5f52f75c749b53d82335c4d307a12f8e0
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-12-20 00:26:51 +0800

    Arranged the alignment of the navbar in the child templates

---
+++ Front_end/online-courses-html-template/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def profile():
+    return render_template("teacher_manage_resource.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
